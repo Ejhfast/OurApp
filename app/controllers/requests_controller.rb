c0180d45b87b5e7dc9688f8c244c88dfd8df01f8
Ethan Fast <ethan@arch.virginia.edu>
2010-02-05 03:31:15 -0500

    request functionality

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,23 +1,48 @@
 class RequestsController < ApplicationController
   def new
+    @req = Request.new
   end
 
   def edit
+    @req = Request.find(params[:id])
   end
 
   def show
+    @req = Request.find(params[:id])
   end
 
   def create
+    @req = Request.new(params[:request])
+    success = @req && @req.save
+    if success && @req.errors.empty?
+      redirect_to requests_path
+      flash[:notice] = "Request created!"
+    else
+      flash[:error] = "We couldn't create your request, sorry."
+      render :action => 'new'
+    end
   end
 
   def update
+    @req = Request.find(params[:id])
+    success = @req && @req.update_attributes(params[:request])
+    if success && @req.errors.empty?
+      redirect_to requests_path
+      flash[:notice] = "Request updated!"
+    else
+      flash[:error] = "We couldn't update your request, sorry."
+      render :action => 'new'
+    end
   end
 
-  def delete
+  def destroy
+    @req = Request.find(params[:id])
+    @req.destroy
+    redirect_to requests_path
   end
 
   def index
+    @reqs = Request.all
   end
 
 end
