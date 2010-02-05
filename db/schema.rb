4433550c329748a7d2468a9ead86c1ed81d9fe81
Ethan Fast <ethan@arch.virginia.edu>
2010-02-05 13:40:28 -0500

    database inconsistency

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100206215034) do
+ActiveRecord::Schema.define(:version => 20100205153038) do
 
   create_table "comments", :force => true do |t|
     t.string   "title",          :null => false
