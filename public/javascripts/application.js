c360a81b19b7ba3094171ee7c1f9807ce0381b7d
Ethan Fast <ethan@arch.virginia.edu>
2010-02-20 19:10:52 -0500

    copyright

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,49 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+void do_canvas(){
+	int radius = 50.0;  
+	int X, Y;  
+	int nX, nY;  
+	int delay = 16;  
+
+	// Setup the Processing Canvas  
+	void setup(){  
+	  size( 200, 200 );  
+	  strokeWeight( 10 );  
+	  frameRate( 15 );  
+	  X = width / 2;  
+	  Y = height / 2;  
+	  nX = X;  
+	  nY = Y;    
+	}  
+
+	// Main draw loop  
+	void draw(){  
+
+	  radius = radius + sin( frameCount / 4 );  
+
+	  // Track circle to new destination  
+	  X+=(nX-X)/delay;  
+	  Y+=(nY-Y)/delay;  
+
+	  // Fill canvas grey  
+	  background( 100 );  
+
+	  // Set fill-color to blue  
+	  fill( 0, 121, 184 );  
+
+	  // Set stroke-color white  
+	  stroke(255);   
+
+	  // Draw circle  
+	  ellipse( X, Y, radius, radius );                    
+	}  
+
+
+	// Set circle's next destination  
+	void mouseMoved(){  
+	  nX = mouseX;  
+	  nY = mouseY;    
+	}
+}
